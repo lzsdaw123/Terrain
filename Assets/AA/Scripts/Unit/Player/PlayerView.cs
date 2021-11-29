94a4cc75624f836d6030ed323b0a41dc4b506c48
OmnisHymmnos <blackcat10205@gmail.com>
2021-11-30 00:28:38 +0800

    目標UI跟隨 11/30 (未完成)
    --新增--
    左輪UV--新增左輪UV
    
    --修正--
    目標UI跟隨--目標UI會根據與畫面方向位移 (未完成)
    左輪模組--替換左輪模組

--- Assets/AA/Scripts/Unit/Player/PlayerView.cs
+++ Assets/AA/Scripts/Unit/Player/PlayerView.cs
@@ -11,7 +11,7 @@ public class PlayerView : MonoBehaviour
     float curtTime = 0f;
     float lastTime = 0f;
     public Camera Camera;
-    public Transform target;
+    public Transform camTransform;
     public float Rdot;
     public float Fdot;
     public Image targetUI;
@@ -24,7 +24,7 @@ public class PlayerView : MonoBehaviour
     }
     public bool IsInView(Vector3 worldPos)
     {
-        Transform camTransform = Camera.transform;  //相機座標
+        camTransform = Camera.transform;  //相機座標
         Vector2 viewPos = Camera.WorldToViewportPoint(worldPos);  //世界座標到視口座標
         Vector3 dir = (worldPos - camTransform.position).normalized;
         float dot = Vector3.Dot(camTransform.forward, dir);     //判斷物體是否在相機前面
@@ -37,51 +37,76 @@ public class PlayerView : MonoBehaviour
     }
     void Update()
     {
-        Vector2 vec2 = Camera.WorldToScreenPoint(this.gameObject.transform.position);  //世界座標到螢幕座標
+        //Vector2 vec2 = Camera.WorldToScreenPoint(this.gameObject.transform.position);  //世界座標到螢幕座標
+        
 
         if (IsInView(transform.position))
         {
             //Debug.Log("目前本物體在攝像機範圍內");
+            Vector2 viewPos = Camera.WorldToViewportPoint(this.gameObject.transform.position);  //世界座標到視口座標
+            Vector2 ScreenPos = Camera.ViewportToScreenPoint(viewPos);  //視口座標→螢幕座標
+            Vector2 SP = new Vector2(ScreenPos.x - 960, ScreenPos.y - 480);
+            targetUI.GetComponent<RectTransform>().anchoredPosition3D = SP;
         }
         else
         {
             //Debug.Log("目前本物體不在攝像機範圍內");
-        }
-        target = Camera.transform;
-        Vector3 dir = transform.position - target.position; //位置差，方向  
-        //方式1   點乘  
-        //點積的計算方式爲: a·b =| a |·| b | cos < a,b > 其中 | a | 和 | b | 表示向量的模 。  
+            Vector3 viewPos = Camera.WorldToViewportPoint(this.gameObject.transform.position);  //世界座標到視口座標
+            Vector3 tUIPosUp = targetUI.GetComponent<RectTransform>().up;
+            Vector3 tUIPosRight = targetUI.GetComponent<RectTransform>().right;
 
-        Rdot = Vector3.Dot(target.right, dir.normalized);//點乘判斷左右： dot1>0在右，<0在左
-        Fdot = Vector3.Dot(target.forward, dir.normalized);//點乘判斷前後：dot >0在前，<0在後
+            camTransform = Camera.transform; //相機座標
+            Vector3 dirForward = (transform.position - camTransform.position).normalized;
+            float dot = Vector3.Dot(camTransform.forward, dirForward);     //判斷物體是否在相機前面
 
-        UI_x = 1920 * Rdot;
-        UI_y = 1080 * Fdot;
+            Transform camT = Camera.transform;
+            camT.position = targetUI.GetComponent<RectTransform>().pivot;
+            Vector3 dir = viewPos - camT.position; //位置差，方向  
+            //Vector3 dir = transform.position - camtarget.position; //位置差，方向  
+            //方式1   點乘  
+            //點積的計算方式爲: a·b =| a |·| b | cos < a,b > 其中 | a | 和 | b | 表示向量的模 。  
+            Rdot = Vector3.Dot(tUIPosRight, dir);//點乘判斷左右： Rdot >0在右，<0在左
+            Fdot = Vector3.Dot(tUIPosUp, dir);//點乘判斷前後：Fdot >0在前，<0在後
+            //Rdot = Vector3.Dot(camtarget.right, dir.normalized);//點乘判斷左右： Rdot >0在右，<0在左
+            //Fdot = Vector3.Dot(camtarget.forward, dir.normalized);//點乘判斷前後：Fdot >0在前，<0在後
 
-        if(UI_x<=-900)  UI_x = -900;
-        else if(UI_x >= 900) UI_x = 900;
+            UI_x = 1920 * Rdot;
+            UI_y = 1080 * Fdot;
+            //畫面左右邊界
+            if (UI_x <= -900) UI_x = -900;
+            else if (UI_x >= 900) UI_x = 900;
+            //畫面上下邊界
+            if (UI_y <= -350) UI_y = -350;
+            else if (UI_y >= 350) UI_y = 350;
 
-        if (UI_y <= -350) UI_y = -350;
-        else if (UI_y >= 350) UI_y = 350;
+            if (dot >= 0)
+            {
+                targetUI.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(UI_x, UI_y, 0);
+            }
+            else
+            {
+                UI_y *= -1;
+                float posY = UI_y / UI_x;
+                UI_y = UI_y * posY;
+                targetUI.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(UI_x*-1, UI_y, 0);
+            }
+        }
 
-        //60 /1860
-        //190 /890
-        if (Fdot > 0)
-        {
-            print("前面");
-        }
-        else if (Fdot < 0)
-        {
-            print("後面");
-        }
-        if (Rdot > 0)
-        {
-            print("R右邊");
-        }
-        else if (Rdot < 0)
-        {
-            print("L左邊");
-        }
-        targetUI.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(UI_x, UI_y, 0);
+        //if (Fdot > 0)
+        //{
+        //    print("前面");
+        //}
+        //else if (Fdot < 0)
+        //{
+        //    print("後面");
+        //}
+        //if (Rdot > 0)
+        //{
+        //    print("R右邊");
+        //}
+        //else if (Rdot < 0)
+        //{
+        //    print("L左邊");
+        //}
     }
 }
